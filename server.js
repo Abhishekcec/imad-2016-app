4e0b21c6e59c61ba0cadf7792988511fb2ecefc6
Abhishekcec <abhisheksugathan002@gmail.com>
2016-11-02 19:40:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
-/*var Pool = require('pg').Pool;
+var Pool = require('pg').Pool;
 
 var config = {
     user: 'abhishekcec',
@@ -10,7 +9,7 @@ var config = {
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
-};*/
+};
 
  
 var app = express();
@@ -123,16 +122,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-/*var pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) { 
-    pool.query('SELECT * FROM test', function(err, result){
+    pool.query('SELECT * FROM test', function(err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
             res.send(JSON.stringify(result));
         }
     });
-});*/
+});
 
 var counter = 0;
 app.get('/counter', function(req, res) {
